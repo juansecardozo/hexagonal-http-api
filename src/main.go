d7d4d3d3b3f2f1aa808dbcdc5536966653c876e6
Sebastian Cardozo <sebastian.cardozo97@gmail.com>
2022-04-19 17:40:12 -0500

    Using gin framework

--- src/main.go
+++ src/main.go
@@ -1,1 +1,22 @@
-package src
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+const httpPort = ":8080"
+
+func main() {
+	println("Server running on", httpPort)
+
+	srv := gin.New()
+	srv.GET("/health", healthHandler)
+
+	log.Fatal(srv.Run(httpPort))
+}
+
+func healthHandler(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "Everything is OK!")
+}
