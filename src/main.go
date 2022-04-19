53fa5c17af24338bafb3ea6ee196412ff4ba4d0b
Sebastian Cardozo <sebastian.cardozo97@gmail.com>
2022-04-19 17:30:23 -0500

    First endpoint

--- src/main.go
+++ src/main.go
@@ -1,1 +1,22 @@
-package src
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const httpPort = ":8080"
+
+func main() {
+	println("Server running on", httpPort)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+
+	log.Fatal(http.ListenAndServe(httpPort, mux))
+}
+
+func healthHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Everything is OK!"))
+}
