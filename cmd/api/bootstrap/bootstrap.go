f30285d31d87e6ddf4e4d1c5a1f30b4e46fa2022
Sebastian Cardozo <sebastian.cardozo97@gmail.com>
2022-05-04 19:53:49 -0500

    Env variables support

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -11,24 +11,18 @@ import (
 	"github.com/juansecardozo/hexagonal-http-api/internal/platform/bus/inmemory"
 	"github.com/juansecardozo/hexagonal-http-api/internal/platform/server"
 	"github.com/juansecardozo/hexagonal-http-api/internal/platform/storage/mysql"
+	"github.com/kelseyhightower/envconfig"
 	"time"
 )
 
-const (
-	host            = "localhost"
-	port            = 8080
-	shutdownTimeout = 10 * time.Second
-
-	dbUser    = "demo"
-	dbPass    = "demo"
-	dbHost    = "localhost"
-	dbPort    = "6603"
-	dbName    = "demo"
-	dbTimeout = 5 * time.Second
-)
-
 func Run() error {
-	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	var cfg config
+	err := envconfig.Process("MOOC", &cfg)
+	if err != nil {
+		return err
+	}
+
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DbUser, cfg.DbPass, cfg.DbHost, cfg.DbPort, cfg.DbName)
 	db, err := sql.Open("mysql", mysqlURI)
 	if err != nil {
 		return err
@@ -39,7 +33,7 @@ func Run() error {
 		eventBus   = inmemory.NewEventBus()
 	)
 
-	courseRepository := mysql.NewCourseRepository(db, dbTimeout)
+	courseRepository := mysql.NewCourseRepository(db, cfg.DbTimeout)
 
 	creatingCourseService := creating.NewCourseService(courseRepository, eventBus)
 	increasingCourseService := increasing.NewCourseCounterIncreaseService()
@@ -52,6 +46,20 @@ func Run() error {
 		creating.NewIncreaseCoursesCounterOnCourseCreated(increasingCourseService),
 	)
 
-	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus)
+	ctx, srv := server.New(context.Background(), cfg.Host, cfg.Port, cfg.ShutdownTimeout, commandBus)
 	return srv.Run(ctx)
+}
+
+type config struct {
+	// Server configuration
+	Host            string        `default:"localhost"`
+	Port            uint          `default:"8080"`
+	ShutdownTimeout time.Duration `default:"10s"`
+	// Database configuration
+	DbUser    string        `default:"demo"`
+	DbPass    string        `default:"demo"`
+	DbHost    string        `default:"localhost"`
+	DbPort    uint          `default:"6603"`
+	DbName    string        `default:"demo"`
+	DbTimeout time.Duration `default:"5s"`
 }
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
